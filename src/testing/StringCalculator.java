4e83c94ad247e5c39be6a89e277e048290ea87c0
unknown <sagardhoot56@gmail.com>
2022-09-07 23:03:13 +0530

    Added Single String Input Condition

--- src/testing/StringCalculator.java
+++ src/testing/StringCalculator.java
@@ -2,10 +2,22 @@ package testing;
 
 public class StringCalculator {
 
-	
+	private boolean isEmpty(String input)
+	{
+		int len=input.length();
+		if(len==0)
+		{
+			return true;
+		}
+		return false;
+	}
 	public int stringcalculate(String input)
 	{
-		return 0;
+		if(isEmpty(input))
+		{
+			return 0; 
+		}
+		return Integer.parseInt(input);
 	}
 //	public static void main(String[] args) {
 //		// TODO Auto-generated method stub
--- src/testing/junit_test.java
+++ src/testing/junit_test.java
@@ -2,14 +2,24 @@ package testing;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class junit_test {
-
+	
+	private StringCalculator calculate;
+	@Before
+	public void init()
+	{
+		calculate=new StringCalculator();
+	}
 	@Test
-	public void EmptyStringTest() {
-		StringCalculator calculate=new StringCalculator();
+	public void emptystringtestreturnszero() {
 		assertEquals(calculate.stringcalculate(""),0);
+	}
+	@Test
+	public void singlestringtestreturnsinteger() {
+		assertEquals(calculate.stringcalculate("100"),100);
 	}
 
 }
